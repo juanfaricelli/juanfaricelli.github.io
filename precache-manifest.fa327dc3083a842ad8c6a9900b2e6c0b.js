661a89f3852d432703cdc5ef97f413a86f13cd97
Juan Faricelli <juan.faricelli@outlook.com>
2019-04-25 21:44:30 -0300

    Updates

---
+++ precache-manifest.fa327dc3083a842ad8c6a9900b2e6c0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9075965bbaadb197549ef561bd44b3d6",
+    "url": "/static/media/arrow-small-dark.9075965b.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5f21b3a45dc405ae2391",
+    "url": "/static/js/main.28aa8fca.chunk.js"
+  },
+  {
+    "revision": "57fdb9447d082bef08ed",
+    "url": "/static/js/2.e0dfff6b.chunk.js"
+  },
+  {
+    "revision": "5f21b3a45dc405ae2391",
+    "url": "/static/css/main.726ef107.chunk.css"
+  },
+  {
+    "revision": "57fdb9447d082bef08ed",
+    "url": "/static/css/2.4dd95840.chunk.css"
+  },
+  {
+    "revision": "caf2fe71a56bda6511b73d8fa5b7339f",
+    "url": "/index.html"
+  }
+];
